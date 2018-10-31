fcec18230af396c59e86b8319316bb4bbcb577d1
amk91 <agostino.pisanelli@gmail.com>
2018-10-30 21:34:46 -0700

    Program won't stop if the generated update folders exist already. Added main loop that checks the folder for new zip files and open the archive if a zip file exists

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 use std::path::{Path, PathBuf};
-use std::io::{BufReader, BufRead};
+use std::io::{BufReader, BufRead, ErrorKind};
 use std::fs::{File, DirBuilder};
 use std::env::current_dir;
+use std::process::exit;
+
+extern crate walkdir;
+use walkdir::WalkDir;
+
+extern crate zip;
+use zip::ZipArchive;
 
 fn main() {
 	// Set environment variables
@@ -11,15 +18,16 @@ fn main() {
 	].iter().collect();
 
 	if !Path::new(&config_file_path).exists() {
-		println!("Config file doesn't exist");
-		std::process::exit(1);
+		println!("Config file doesn't exist in {}",
+			current_dir().unwrap_or(PathBuf::new()).to_str().unwrap());
+		exit(1);
 	}
 
 	let config_file = if let Ok(file) = File::open(&config_file_path) {
 		file
 	} else {
 		println!("Unable to open {}", config_file_path.to_str().unwrap_or(""));
-		std::process::exit(1);
+		exit(1);
 	};
 
 	let config_file = BufReader::new(&config_file);
@@ -93,12 +101,16 @@ fn main() {
 
 		let auto_updater_dir_path = format!("{}__auto_updater\\", value);
 		if let Err(err) = DirBuilder::new().create(&auto_updater_dir_path) {
-			panic!("Error creating folder, {}", err);
+			if err.kind() != ErrorKind::AlreadyExists {
+				panic!("Error creating folder, {}", err);
+			}
 		}
 
 		let auto_updater_history_dir_path = format!("{}__auto_updater_history\\", value);
 		if let Err(err) = DirBuilder::new().create(&auto_updater_history_dir_path) {
-			panic!("Error creating folder, {}", err);
+			if err.kind() != ErrorKind::AlreadyExists {
+				panic!("Error creating folder, {}", err);
+			}
 		}
 
 		(auto_updater_history_dir_path, auto_updater_dir_path)
@@ -115,4 +127,43 @@ fn main() {
 	} else {
 		panic!("No backup dir given");
 	};
+
+	'main: loop {
+		for entry in WalkDir::new(&update_dir_path).into_iter().filter_map(|e| e.ok()) {
+			if let Some(extension) = entry.path().extension() {
+				let archive_file_path = if extension == "zip" {
+					entry.path().to_str().unwrap_or("")
+				} else {
+					continue;
+				};
+
+				let mut archive = if let Ok(archive_file) = File::open(&archive_file_path) {
+					if let Ok(archive) = ZipArchive::new(archive_file) {
+						archive
+					} else {
+						println!("Unable to open zip file {}", archive_file_path);
+
+						//TODO: move the file into a different folder
+
+						continue;
+					}
+				} else {
+					println!("Unable to open file {}", archive_file_path);
+
+					//TODO: move the file into a different folder
+
+					continue;
+				};
+
+				for i in 0..archive.len() {
+					let mut file = if let Ok(file) = archive.by_index(i) {
+						file
+					} else {
+						println!("Unable to open file inside the archive");
+						break;
+					};
+				}
+			}
+		}
+	}
 }
